76e81945e253dcd0826b7e43fd06e899e2d2b4bb
jsebasct <jsebasct@gmail.com>
2021-05-13 17:51:31 -0300

    feat: TaskController createTask

--- src/main/kotlin/org/ground/play/controller/TaskController.kt
+++ src/main/kotlin/org/ground/play/controller/TaskController.kt
@@ -2,6 +2,7 @@ package org.ground.play.controller
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import org.ground.play.model.Task
 import org.ground.play.service.TaskService
 import javax.inject.Inject
@@ -14,7 +15,12 @@ class TaskController() {
 
     @Get("/")
     fun getAll(): List<Task> {
-        return taskService.getAll()
+        return taskService.getTasks()
+    }
+
+    @Post
+    fun createTask(task: Task): Task {
+        return taskService.createTask(task)
     }
 
 }
--- src/main/kotlin/org/ground/play/service/TaskService.kt
+++ src/main/kotlin/org/ground/play/service/TaskService.kt
@@ -5,9 +5,14 @@ import javax.inject.Singleton
 
 @Singleton
 class TaskService {
-    private val tasks = List(10) { Task("Task $it", 1) }
+    private val tasks = MutableList(5) { Task("Task $it", it+1) }
 
-    fun getAll(): List<Task> {
+    fun getTasks(): List<Task> {
         return tasks
+    }
+
+    fun createTask(task: Task): Task {
+        tasks.add(task)
+        return task
     }
 }
