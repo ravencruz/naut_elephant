cbbbf73dad9fac0b1c5036fa8ef4886ee27ad084
jsebasct <jsebasct@gmail.com>
2021-05-17 16:41:05 -0300

    docs: sample github controller http client

--- src/main/kotlin/org/ground/play/common/route/GithubApiClient.kt
+++ src/main/kotlin/org/ground/play/common/route/GithubApiClient.kt
@@ -1,4 +1,22 @@
 package org.ground.play.common.route
 
+import io.micronaut.http.HttpHeaders.ACCEPT
+import io.micronaut.http.HttpHeaders.USER_AGENT
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Header
+import io.micronaut.http.annotation.Headers
+import io.micronaut.http.client.annotation.Client
+import io.reactivex.Flowable
+import org.ground.play.beans.GithubRelease
+import org.ground.play.common.configuration.GithubConfiguration
+
+@Client(GithubConfiguration.GITHUB_API_URL)
+@Headers(
+    Header(name = USER_AGENT, value = "Micronaut HTTP Client"),
+    Header(name = ACCEPT, value = "application/vnd.github.v3+json, application/json"),
+)
 interface GithubApiClient {
+
+    @Get("/repos/\${github.organization}/\${github.repo}/releases")
+    fun fetchReleases(): Flowable<GithubRelease?>?
 }
--- src/main/kotlin/org/ground/play/controller/GithubController.kt
+++ src/main/kotlin/org/ground/play/controller/GithubController.kt
@@ -1,17 +1,25 @@
 package org.ground.play.controller
 
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.reactivex.Flowable
 import io.reactivex.Maybe
 import org.ground.play.beans.GithubRelease
+import org.ground.play.common.route.GithubApiClient
 import org.ground.play.common.route.GithubLowLevelClient
 
 @Controller("/github")
-class GithubController (private val githubLowLevelClient: GithubLowLevelClient) {
+class GithubController (private val githubLowLevelClient: GithubLowLevelClient,
+                        private val githubApiClient: GithubApiClient) {
 
     @Get("/releases-low-level")
     fun releases(): Maybe<List<GithubRelease>> {
         return githubLowLevelClient.fetchReleases()
     }
 
+    @Get(uri = "/releases", produces = [MediaType.APPLICATION_JSON_STREAM])
+    fun fetchReleases(): Flowable<GithubRelease?>? {
+        return githubApiClient.fetchReleases()
+    }
 }
