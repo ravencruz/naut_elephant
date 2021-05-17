c6bd37074ed3dd76966be455937423f31a4400f5
jsebasct <jsebasct@gmail.com>
2021-05-17 10:17:54 -0300

    refactor: custom exception handler

--- src/main/kotlin/org/ground/play/common/exception/OutOfStockExceptionHandler.kt
+++ src/main/kotlin/org/ground/play/common/exception/OutOfStockExceptionHandler.kt
@@ -4,6 +4,9 @@ import io.micronaut.context.annotation.Requirements
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpResponseFactory
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Produces
 import io.micronaut.http.server.exceptions.ExceptionHandler
 import io.micronaut.http.server.exceptions.response.ErrorContext
@@ -19,12 +22,15 @@ class TaskExceptionHandler
     (private val errorResponseProcessor: ErrorResponseProcessor<Any>)
     : ExceptionHandler<TaskException, HttpResponse<*>>
 {
+    private fun <T> setStatus(status: HttpStatus): MutableHttpResponse<T>? {
+        return HttpResponseFactory.INSTANCE.status(status)
+    }
 
     override fun handle(request: HttpRequest<*>, exception: TaskException): HttpResponse<*> {
         return errorResponseProcessor.processResponse(
             ErrorContext.builder(request)
                 .cause(exception)
                 .errorMessage(exception.message)
-                .build(), HttpResponse.badRequest<Any>())
+                .build(), setStatus<Any>(exception.status))
     }
 }
